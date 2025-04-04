c78a9bb5a3b74d53618e59eed873c52d4805a550
yunzivv <yunzivv@gmail.com>
2025-04-04 23:52:32 +0900

    프로그래머스 자릿수 더하기

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,22 +1,13 @@
 package org.example;
 
-// 프로그래머스 n의 배수 구하기
+// 프로그래머스 자릿수 더하기
 
 class Solution {
-    public int[] solution(int n, int[] numlist) {
-        int a = 0;
-        for(int number : numlist) {
-            if(number % n == 0) {
-                a++;
-            }
-        }
-        int[] answer = new int[a];
-        int i = 0;
-        for(int number : numlist) {
-            if(number % n == 0) {
-                answer[i] = number;
-                i ++;
-            }
+    public int solution(int n) {
+        int answer = 0;
+        while(n > 0) {
+            answer += n % 10;
+            n /= 10;
         }
         return answer;
     }
