5094f49b3cf2297985babe2ba32037dc086f64cb
yunzivv <yunzivv@gmail.com>
2025-04-04 23:44:25 +0900

    프로그래머스 몫 구하기

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,11 +1,11 @@
 package org.example;
 
-// 프로그래머스 나이 출력
+// 프로그래머스 몫 구하기
 
 class Solution {
-    public int solution(int age) {
+    public int solution(int num1, int num2) {
         int answer = 0;
-        answer = 2022 - age + 1;
+        answer = num1 / num2;
         return answer;
     }
 }
