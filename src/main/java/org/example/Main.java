3ab9e7b179aacbb4e03a996287bba1fdd481e254
yunzivv <yunzivv@gmail.com>
2025-04-04 23:30:48 +0900

    프로그래머스 모음 제거

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
-// 프로그래머스 입문 점의 위치 구하기
 
 class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.solution(new int[]{1,2,3,4,5});
     }
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,19 +1,16 @@
 package org.example;
 
+// 프로그래머스 모음 제거
+
 class Solution {
-    public int solution(int[] numbers) {
-        int answer = 0;
-        int temp = 0;
-        for(int i = 0; i < numbers.length - 1; ++i) {
-            for(int j = i+1; j < numbers.length; ++j) {
-                if(numbers[i] < numbers[j]) {
-                    temp = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = temp;
-                }
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i = 0; i < my_string.length(); i++) {
+            char aeiou = my_string.charAt(i);
+            if(aeiou != 'a' && aeiou != 'e' && aeiou != 'i' && aeiou != 'o' && aeiou != 'u' ){
+                answer += aeiou;
             }
         }
-        answer = numbers[0] * numbers[1];
         return answer;
     }
 }
