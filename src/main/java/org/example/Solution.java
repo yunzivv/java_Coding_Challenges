39c1a5a066f66204ca4df2dadb2bd4f044f55f18
yunzivv <yunzivv@gmail.com>
2025-04-04 23:50:15 +0900

    프로그래머스 n의 배수 구하기

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,11 +1,23 @@
 package org.example;
 
-// 프로그래머스 몫 구하기
+// 프로그래머스 n의 배수 구하기
 
 class Solution {
-    public int solution(int num1, int num2) {
-        int answer = 0;
-        answer = num1 / num2;
+    public int[] solution(int n, int[] numlist) {
+        int a = 0;
+        for(int number : numlist) {
+            if(number % n == 0) {
+                a++;
+            }
+        }
+        int[] answer = new int[a];
+        int i = 0;
+        for(int number : numlist) {
+            if(number % n == 0) {
+                answer[i] = number;
+                i ++;
+            }
+        }
         return answer;
     }
 }
