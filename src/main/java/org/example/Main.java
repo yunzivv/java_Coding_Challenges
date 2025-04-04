38d936599839662f046267e58f5fd65859b2407b
yunzivv <yunzivv@gmail.com>
2025-04-04 22:56:22 +0900

    프로그래머스 점의 위치 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,21 @@
 package org.example;
 
-// 프로그래머스 입문 배열 자르기
+// 프로그래머스 입문 점의 위치 구하기
 
 class Solution {
-    public int[] solution(int[] numbers, int num1, int num2) {
-        int a = num2 - num1 + 1;
-        int index = 0;
-        int[] answer = new int[a];
-        for(int i = num1; i <= num2; i++){
-            answer[index] = numbers[i];
-            index++;
+    public int solution(int[] dot) {
+        int answer = 0;
+        boolean x = dot[0] > 0;
+        boolean y = dot[1] > 0;
+
+        if(x && y) {
+            answer = 1;
+        } else if(x == false && y == true) {
+            answer = 2;
+        } else if(x == false && y == false) {
+            answer = 3;
+        } else {
+            answer = 4;
         }
         return answer;
     }
