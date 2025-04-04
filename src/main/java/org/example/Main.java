acc56c92a193eb3fa356653fec7fa179cebad4ca
yunzivv <yunzivv@gmail.com>
2025-04-04 21:03:38 +0900

    프로그래머스 피자 나눠 먹기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,13 @@ package org.example;
 
 // java 코딩 챌린지 세팅
 
-public class Main {
-    public static void main(String[] args) {
-
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 1;
+        while(n > slice) {
+            n -= slice;
+            answer++;
+        }
+        return answer;
     }
 }
