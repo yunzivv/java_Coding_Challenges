c8da7b2de60abf4b9e0017c825b1c2542f49f83b
yunzivv <yunzivv@gmail.com>
2025-04-05 10:23:15 +0900

    프로그래머스 제곱수 판별하기

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,6 +1,6 @@
 package org.example;
 
-// 프로그래머스 순서쌍의 개수
+// 프로그래머스 제곱수 판별하기
 
 class Solution {
     public int solution(int n) {
@@ -9,6 +9,11 @@ class Solution {
             if(n % i == 0) {
                 answer++;
             }
+        }
+        if(answer % 2 == 0) {
+            answer = 2;
+        } else {
+            answer = 1;
         }
         return answer;
     }
