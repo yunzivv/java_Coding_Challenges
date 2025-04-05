f57ae36b1911ae1714ef4620344200328b923184
yunzivv <yunzivv@gmail.com>
2025-04-05 10:59:32 +0900

    프로그래머스 이어 붙인 수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 
+import javax.sound.midi.Soundbank;
+
 class Main {
     public static void main(String[] args) {
+        int[]a = {3,4,5,2,1};
+        System.out.println(new Solution().solution(a));
     }
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,14 +1,20 @@
 package org.example;
 
-// 프로그래머스 피자 나눠 먹기(1)
+// 프로그래머스 이어 붙인 수
 
 class Solution {
-    public int solution(int n) {
-        int answer = 1;
-        while(n > 7) {
-            answer++;
-            n -= 7;
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int even = 0;
+        int odd = 0;
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] % 2 == 0) {
+                even = even * 10 + num_list[i];
+            } else {
+                odd = odd * 10 + num_list[i];
+            }
         }
+        answer = even + odd;
         return answer;
     }
 }
