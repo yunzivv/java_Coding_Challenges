c0eab7d2c275b0e0531ee21918221c70d1ff23d6
yunzivv <yunzivv@gmail.com>
2025-04-06 22:54:19 +0900

    프로그래머스 직각삼각형 출력하기

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,16 +1,17 @@
 package org.example;
 
-// 프로그래머스 옷가게 할인 받기
-class Solution {
-    public int solution(int price) {
-        int answer = price;
-        if(price >= 500000) {
-            answer = (int)(price * 0.8);
-        } else if(price >= 300000) {
-            answer = (int)(price * 0.9);
-        } else if(price >= 100000) {
-            answer = (int)(price * 0.95);
+// 프로그래머스 직각삼각형 출력하기
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j <= i + 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        return answer;
     }
 }
