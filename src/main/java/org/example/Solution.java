e10e819069df2b2c9e565d706c56d3c11134868f
yunzivv <yunzivv@gmail.com>
2025-04-06 20:29:43 +0900

    프로그래머스 문자열 곱하기

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,20 +1,13 @@
 package org.example;
 
-// 프로그래머스 이어 붙인 수
+// 프로그래머스 문자열 곱하기
 
 class Solution {
-    public int solution(int[] num_list) {
-        int answer = 0;
-        int even = 0;
-        int odd = 0;
-        for(int i = 0; i < num_list.length; i++) {
-            if(num_list[i] % 2 == 0) {
-                even = even * 10 + num_list[i];
-            } else {
-                odd = odd * 10 + num_list[i];
-            }
+    public String solution(String my_string, int k) {
+        String answer = "";
+        for(int i = 0; i < k; i++) {
+            answer += my_string;
         }
-        answer = even + odd;
         return answer;
     }
 }
