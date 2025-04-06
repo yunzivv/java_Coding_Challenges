b8b7fa5105630ebb64b60ed8e5c410ab89603f6f
yunzivv <yunzivv@gmail.com>
2025-04-06 22:43:54 +0900

    프로그래머스 중앙 값 구하기

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,12 +1,20 @@
 package org.example;
 
-// 프로그래머스 배열 두배 만들기
+// 프로그래머스 중앙 값 구하기
 class Solution {
-    public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
-        for(int i = 0; i < numbers.length; i++){
-            answer[i] = numbers[i]*2;
+    public int solution(int[] array) {
+        int answer = 0;
+        int temp;
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length; j++){
+                if(array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
         }
+        answer = array[array.length / 2];
         return answer;
     }
 }
