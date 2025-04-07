5720a9adf17b7e63199aafb2544da87193b4d3d4
yunzivv <yunzivv@gmail.com>
2025-04-07 20:31:19 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.71 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        for(int i = 0; i < my_string.length(); i++){
+            if(my_string.charAt(i) - 48 >= 0 && my_string.charAt(i)-48 <= 9) {
+                System.out.println((int)my_string.charAt(i)-48);
+                answer += (int)my_string.charAt(i)-48;
+            }
+        }
+        return answer;
+    }
+}
