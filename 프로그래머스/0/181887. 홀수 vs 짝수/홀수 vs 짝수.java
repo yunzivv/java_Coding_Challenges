4e9bf54226a59ce1c48e7c67633f320644bcbbe2
yunzivv <yunzivv@gmail.com>
2025-04-09 23:52:51 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.04 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int even = 0;
+        int odd = 0;
+        for(int i = 0; i < num_list.length; i++){
+            if(i % 2 == 0){
+                even += num_list[i];
+            } else {
+                odd += num_list[i];
+            }
+        }
+        
+        if(even > odd){
+            answer = even;
+        } else {
+            answer = odd;
+        }
+        return answer;
+    }
+}
