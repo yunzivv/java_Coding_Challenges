119204930638e16ca369c4f1fd518022cc8172cb
yunzivv <yunzivv@gmail.com>
2025-04-11 23:15:36 +0900

    [level 0] Title: l로 만들기, Time: 1120.07 ms, Memory: 393 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i = 0; i < myString.length(); i++){
+            char c = myString.charAt(i);
+            if(c < 'l'){
+                c = 'l';
+            }
+            answer += c;
+        }
+        return answer;
+    }
+}
