7b551f52b45a4ef2ac49ac9345ffc36ba098d645
yunzivv <yunzivv@gmail.com>
2025-04-14 22:01:47 +0900

    [level 0] Title: 수 조작하기 2, Time: 1110.71 ms, Memory: 377 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        for(int i = 0; i < numLog.length - 1; i++){
+            if(numLog[i] + 1 == numLog[i + 1]){
+                answer += "w";
+            } else if(numLog[i] - 1 == numLog[i + 1]){
+                answer += "s";
+            } else if(numLog[i] + 10 == numLog[i + 1]){
+                answer += "d";   
+            } else {
+                answer += "a";
+            }
+        }
+        return answer;
+    }
+}
