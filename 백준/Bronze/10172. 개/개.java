40ac8f86b377d5500724ea643878930c52d37fdb
yunzivv <yunzivv@gmail.com>
2025-04-27 21:21:52 +0900

    [Bronze V] Title: 개, Time: 160 ms, Memory: 17408 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
