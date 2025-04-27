4774ceed16f9c067ed2e9e7fb7ecc2acf64ea25c
yunzivv <yunzivv@gmail.com>
2025-04-27 21:21:27 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.nextLine();
+
+        System.out.println(A + "??!");
+
+    }
+}
