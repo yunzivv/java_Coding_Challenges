f37fa6c34a298965194015b842d2401f0a29dc92
yunzivv <yunzivv@gmail.com>
2025-04-27 21:21:34 +0900

    [Bronze III] Title: 곱셈, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int hundred = (b / 100); // 100의 자리수 구하기
+        int ten = ((b - hundred*100) / 10); // 10의 자리수 구하기
+        int one = b - hundred * 100 - ten * 10; // 1의 자리수 구하기
+
+        System.out.println(a * one);
+        System.out.println(a * ten);
+        System.out.println(a * hundred);
+        System.out.println(a * b);
+
+
+    }
+}
