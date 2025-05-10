4877f2c4343ab7d1e898fec6b702ba1c43dfdd74
yunzivv <yunzivv@gmail.com>
2025-05-11 04:15:55 +0900

    [Bronze IV] Title: 영수증, Time: 188 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sumprice = sc.nextInt();
+        int totalcount = sc.nextInt();
+        int[] receiptlist = new int[totalcount * 2];
+
+        int checkprice = 0;
+        int lastindex = 0;
+
+        for (int i = 0; i < totalcount * 2; i++) {
+            receiptlist[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < totalcount; i++){
+            checkprice += receiptlist[lastindex] * receiptlist[lastindex+1];
+            lastindex += 2;
+        }
+        if (checkprice == sumprice) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
