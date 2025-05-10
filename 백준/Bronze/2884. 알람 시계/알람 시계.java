72b58284c4b2e5aeb34ae7bf290117f508119f68
yunzivv <yunzivv@gmail.com>
2025-05-10 23:17:39 +0900

    [Bronze III] Title: 알람 시계, Time: 172 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        if (m >= 45) {
+            System.out.println(h);
+            System.out.println(m - 45);
+        }  else {
+            h -= 1;
+            if (h == -1) {
+                h = 23;
+            }
+            m = m + 15;
+            System.out.println(h);
+            System.out.println(m);
+        }
+
+    }
+}
