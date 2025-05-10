e4beff8784c2f0f201d0b8697cfc8d29029b8ce1
yunzivv <yunzivv@gmail.com>
2025-05-10 23:17:44 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int dice1 = sc.nextInt();
+        int dice2 = sc.nextInt();
+        int dice3 = sc.nextInt();
+
+        int prize = 0;
+
+        if (dice1 == dice2 && dice1 == dice3) {
+            prize = 10000 + dice1 * 1000;
+        } else if (dice1 == dice2 || dice2 == dice3 || dice1 == dice3) {
+            if (dice1 == dice3) {
+                prize = 1000 + dice3 * 100;
+            } else {
+                prize = 1000 + dice2 * 100;
+            }
+        } else {
+            if (dice1 > dice2 && dice1 > dice3){
+                prize = dice1 * 100;
+            } else if (dice2 > dice1 && dice2 > dice3) {
+                prize = dice2 * 100;
+            } else if (dice3 > dice1 && dice3 > dice2) {
+                prize = dice3 * 100;
+            }
+        }
+        System.out.println(prize);
+
+    }
+}
