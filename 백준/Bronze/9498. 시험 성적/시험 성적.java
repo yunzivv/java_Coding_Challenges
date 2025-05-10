ca6859680d9140e53bc0a03f971ab070405c07d4
yunzivv <yunzivv@gmail.com>
2025-05-10 23:16:15 +0900

    [Bronze V] Title: 시험 성적, Time: 176 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();
+
+        if (score > 89) {
+            System.out.println("A");
+        } else if (score > 79) {
+            System.out.println("B");
+        } else if (score > 69) {
+        System.out.println("C");
+        } else if (score > 59) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+
+    }
+}
