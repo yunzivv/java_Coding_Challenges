6eb2a8529afcfa9762373d1c698681ecd94b5ebc
yunzivv <yunzivv@gmail.com>
2025-07-20 15:00:17 +0900

    [level 2] Title: 숫자의 표현, Time: 1.61 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        for(int i = 1; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                if(sum >= n) break;
+                sum += j;
+            }
+            if(sum == n) answer++;
+        }
+        return answer;
+    }
+}
