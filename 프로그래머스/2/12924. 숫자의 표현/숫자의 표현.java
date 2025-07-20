378895e021820cfb2dcdb98611580bc849aa5b02
yunzivv <yunzivv@gmail.com>
2025-07-20 15:02:14 +0900

    [level 2] Title: 숫자의 표현, Time: 1.86 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 1;
         for(int i = 1; i < n; i++){
             int sum = 0;
-            for(int j = i; j < n; j++){
+            for(int j = i; j <= n - (n/2); j++){
                 if(sum >= n) break;
                 sum += j;
             }
