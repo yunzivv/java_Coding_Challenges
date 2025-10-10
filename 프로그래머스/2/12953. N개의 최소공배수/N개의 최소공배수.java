09ff104261b500f72870c8d000a78a7d6340faeb
yunzivv <yunzivv@gmail.com>
2025-10-10 13:40:01 +0900

    [level 2] Title: N개의 최소공배수, Time: 6.14 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+        
+        // int product = 1;
+        int big = 0;
+        for (int n : arr) {
+            // product *= n;
+            if(big < n) big = n;
+        }
+        
+        int last = arr[arr.length - 1];
+        for(int i = 1; i < 2147483647; i++){
+            boolean isLCM = LCM(big * i, arr);
+            if(isLCM) return big * i;
+        }
+        return answer;
+    }
+    
+    public boolean LCM(int num, int[] list){
+        
+        for(int i : list){
+            if(num % i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
