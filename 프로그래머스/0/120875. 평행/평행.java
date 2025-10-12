302e22011a5a374ff6e078dff16daf54ae9a5392
yunzivv <yunzivv@gmail.com>
2025-10-12 16:22:41 +0900

    [level 0] Title: 평행, Time: 13.92 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] dots) {
+        
+        if(isParallel(new int[][]{dots[0], dots[1]}, new int[][]{dots[2], dots[3]})) return 1;
+        if(isParallel(new int[][]{dots[0], dots[2]}, new int[][]{dots[1], dots[3]})) return 1;
+        if(isParallel(new int[][]{dots[0], dots[3]}, new int[][]{dots[1], dots[2]})) return 1;
+        
+        return 0;
+    }
+    
+    boolean isParallel(int[][] line1, int[][] line2){
+        // int inc1 = (int) Math.abs((line1[0][0] - line1[1][0]) * (line1[0][1] - line1[1][1]));
+        double inc1 = (double)Math.abs(line1[0][0] - line1[1][0]) / Math.abs(line1[0][1] - line1[1][1]) * 100.0;
+        // int inc2 = (int) Math.abs((line2[0][0] - line2[1][0]) * (line2[0][1] - line2[1][1]));
+        double inc2 = (double)Math.abs(line2[0][0] - line2[1][0]) / Math.abs(line2[0][1] - line2[1][1]) * 100.0;
+        
+        System.out.println(inc1 + " " + inc2);
+        
+        return inc1 == inc2;
+    }
+}
