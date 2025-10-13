479fc1f99d53b595bb253767c140764877151d5f
yunzivv <yunzivv@gmail.com>
2025-10-13 15:24:12 +0900

    [level 0] Title: 가위 바위 보, Time: 3.00 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,15 +1,13 @@
 class Solution {
     public String solution(String rsp) {
         String answer = "";
-        for(int i = 0; i < rsp.length(); i++) {
-            if(rsp.charAt(i) == '0'){
-                answer += "5";
-            } else if(rsp.charAt(i) == '2'){
-                answer += "0";
-            } else {
-                answer += "2";
-            }
+        
+        for(char c : rsp.toCharArray()){
+            if(c == '2') answer += "0";
+            else if(c == '0') answer += "5";
+            else answer += "2";
         }
+        
         return answer;
     }
 }
