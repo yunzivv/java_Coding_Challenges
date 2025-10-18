82e48ba7d4735d7bcca6dc3c4fc6312a57b78958
yunzivv <yunzivv@gmail.com>
2025-10-18 11:32:24 +0900

    [level 2] Title: 피로도, Time: 3.58 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,48 +1,18 @@
-import java.util.*;
-
 class Solution {
-    boolean[] visited = new boolean[9];
-    List<Integer> list = new ArrayList<>();
-    int ans = 0, K = -1;
-    int[][] pDungeons;
-    
-    public void find(int cur, int size) {
-        if (cur == size) {
-            int pirodo = K, stage = 0;
-            for(int num: list) {
-                if (pirodo >= pDungeons[num][0]) {
-                    pirodo -= pDungeons[num][1];
-                    stage++;
-                }
+    int dfs(int k, int[][] dungeons) {
+        int cnt = 0;
+        for(int[] d : dungeons) {
+            int a = d[0], b = d[1];
+            if(a <= k) {
+                d[0] = 9999;
+                cnt = Math.max(1 + dfs(k - b, dungeons), cnt);
+                d[0] = a;
             }
-            
-            ans = Math.max(ans, stage);
-            return;
         }
-        
-        for (int i = 0; i < size; i++) {
-            if (visited[i]) continue;
-            list.add(i);
-            visited[i] = true;
-            
-            find(cur + 1, size);
-            
-            visited[i] = false;
-            list.remove(list.size() -1);   
-        }
+        return cnt;
     }
-    
     public int solution(int k, int[][] dungeons) {
-        int n = dungeons.length;
-        pDungeons = new int[n][2];
-        for(int i = 0; i < n; i++) {
-            pDungeons[i][0] = dungeons[i][0];
-            pDungeons[i][1] = dungeons[i][1];
-        }
-        K = k;
-        
-        find(0, n);
-        
-        return ans;
+        int answer = -1;
+        return dfs(k, dungeons);
     }
 }
