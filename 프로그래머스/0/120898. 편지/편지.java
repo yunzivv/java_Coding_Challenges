e18a7085b848f9d534f8e5d83a65581af2bfaa3e
yunzivv <yunzivv@gmail.com>
2025-10-28 21:52:38 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(String message) {
-        int answer = 0;
-        answer = message.length() * 2;
-        return answer;
+        return message.length() * 2;
     }
 }
