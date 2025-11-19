ae24987d189a27958aa0a21f23e86f4d6efa6c53
yunzivv <yunzivv@gmail.com>
2025-11-20 01:40:39 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 10.16 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,21 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length - 5];
-        int temp = 0;
-        for(int i = 0; i < num_list.length; i++){
-            for(int j = 0; j < num_list.length; j++){
-                if(num_list[i] < num_list[j]){
-                    temp = num_list[i];
-                    num_list[i] = num_list[j];
-                    num_list[j] = temp;
-                }
-            }
-        }
-        int a = 0;
-        for(int i = 5; i < num_list.length; i++){
-            answer[a] = num_list[i];
-            a++;
-        }
-        return answer;
+        return Arrays.stream(num_list).sorted().skip(5).toArray();
     }
 }
