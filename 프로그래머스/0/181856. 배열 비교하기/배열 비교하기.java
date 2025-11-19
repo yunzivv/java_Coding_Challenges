761654380cf26426a7d7111c0cfe5a17b6bfbff9
yunzivv <yunzivv@gmail.com>
2025-11-20 01:38:12 +0900

    [level 0] Title: 배열 비교하기, Time: 0.03 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,25 +1,14 @@
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        int answer = 1;
-        if(arr2.length > arr1.length){
-            answer = -1;
-        } else if (arr2.length == arr1.length){
+        if (arr2.length == arr1.length){
             int one = 0;
-            for(int i = 0; i < arr1.length; i++){
-                one += arr1[i];
-            }
             int two = 0;
             for(int i = 0; i < arr1.length; i++){
+                one += arr1[i];
                 two += arr2[i];
             }
-            if(one > two){
-                answer = 1;
-            } else if(two > one){
-                answer = -1;
-            } else {
-                answer = 0;
-            }
+            return one > two ? 1 : two > one ? -1 : 0;
         }
-        return answer;
+        return arr1.length > arr2.length ? 1 : -1;
     }
 }
