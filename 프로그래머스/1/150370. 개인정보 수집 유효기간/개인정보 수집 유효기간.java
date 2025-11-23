43d428d052e6f1c901368f305d161de4a8868430
yunzivv <yunzivv@gmail.com>
2025-11-23 23:16:02 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 29.87 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        int todayInt = Integer.parseInt(today.replace(".", ""));
+        
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(String s : terms) map.put(s.charAt(0), Integer.parseInt(s.substring(2)));
+        
+        for(int i = 0; i < privacies.length; i++){
+
+            int[] arr = Arrays.stream(privacies[i].substring(0, 10).split("\\."))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+            
+            arr[1] += map.get(privacies[i].charAt(11));
+            System.out.println(arr[0] * 10000 + arr[1] * 100 + arr[2]);
+            
+            if(arr[1] > 12){
+                System.out.print("big : ");
+                arr[0] += arr[1] / 12;
+                arr[1] %= 12;
+                if(arr[1] == 0){
+                    arr[0]--;
+                    arr[1] = 12;
+                }
+            }
+            
+            if(arr[0] * 10000 + arr[1] * 100 + arr[2] <= todayInt) {
+                list.add(i + 1);
+            }
+        }
+        
+        return list.stream()
+                .mapToInt(i -> i)
+                .toArray();
+    }
+}
