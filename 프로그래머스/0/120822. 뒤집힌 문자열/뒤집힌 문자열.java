0fdb776784416e9a22614feff24d11e19e0ae80f
yunzivv <yunzivv@gmail.com>
2025-11-27 22:30:11 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.06 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        for(char c : my_string.toCharArray()){
-            answer = String.valueOf(c) + answer;
-        }
-        return answer;
+        return new StringBuilder(my_string).reverse().toString();
     }
 }
