247f1040dbf34c3f76e41550b8b389bed4315131
yunzivv <yunzivv@gmail.com>
2026-01-21 19:08:43 +0900

    [level 2] Title: 타겟 넘버, Time: 4.71 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,19 @@
+class Solution {
+    int count = 0;
+
+    void dfs(int[] numbers, int target, int idx, int sum) {
+        
+        if (idx == numbers.length) {
+            if(sum == target) count++;
+            return;
+        }
+
+        dfs(numbers, target, idx + 1, sum + numbers[idx]);
+        dfs(numbers, target, idx + 1, sum - numbers[idx]);
+    }
+
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return count;
+    }
+}
