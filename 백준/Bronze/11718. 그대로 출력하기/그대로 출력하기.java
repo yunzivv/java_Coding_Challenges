166023d9cedfd59e758a34c469e070a1963344fd
yunzivv <yunzivv@gmail.com>
2026-01-27 12:26:38 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append('\n');
+        }
+
+        System.out.print(sb.toString());
+    }
+}
