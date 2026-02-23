9a53dba8d728bb6e8ce2899403dd48d783377827
yunzivv <yunzivv@gmail.com>
2026-02-23 21:46:01 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 164 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+        System.out.println(1);
+    }
+}
