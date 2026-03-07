2dff99d4109b05b73a0429ae53cb0aff24a4263c
yunzivv <yunzivv@gmail.com>
2026-03-08 00:35:38 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1828 ms, Memory: 192468 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] dots = new int[n][2];
+
+        for(int i = 0; i < n; i++) {
+            dots[i][0] = sc.nextInt();
+            dots[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(dots, (a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        });
+
+        for (int[] i : dots) {
+            System.out.println(i[0] + " " + i[1]);
+        }
+    }
+}
